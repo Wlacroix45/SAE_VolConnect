e9cf723949e150fe3f06c85e2f1a439018d57863
Gwen-Orl <gwendan.pupp-baudson@etu.univ-orleans.fr>
2026-03-03 11:17:39 +0100

    Initialisation de la bd sqlAlchemy

--- vol/app/myapp.py
+++ vol/app/myapp.py
@@ -1,2 +1,8 @@
 from flask import Flask
+from .extensions import api, db
+
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+api.init_app(app)
+db.init_app(app)
