151eff715091b31e3954dba354abe08b0cab2710
Gwen-Orl <gwendan.pupp-baudson@etu.univ-orleans.fr>
2026-03-13 16:13:12 +0100

    Ajout de la route pour récupérer toutes les compagnies

---
+++ SPA/js/views/pages/CompagnieAll.js
@@ -0,0 +1,56 @@
+import CompagnieProvider from "../../services/CompagnieProvider.js";
+
+export default class CompagnieAll{
+    constructor() {
+        this.page_ac = 1;
+        this.items_per_page = 10;
+        this.total_pages = null;
+    }
+
+    async setPage(newPage) {
+        this.page_ac = newPage;
+        const content = document.querySelector('#main');
+        content.innerHTML = await this.render();
+    }
+
+    async render(){
+        let compagnies = await CompagnieProvider.fetchCompagnies(10, this.page_ac);
+        let view = `
+            <h2 style="text-align: center;">Toutes les compagni</h2>
+            <ul class="list-group">
+                ${compagnies.map(
+                    compagnies => 
+                        `
+                            <li class="list-group-item"><a class="list-group-item list-group-item-action" href="#/terminaux/${compagnies.id_compagnie}">${compagnies.nom_comp}</a></li>
+                    `
+                ).join("\n")}
+             </ul>
+
+            <nav aria-label="Page navigation exemple">
+                <ul class="pagination">
+                    <li class="page-item ${this.page_ac === 1 ? 'disabled' : ''}">
+                        <a class="page-link" href="#" onclick="event.preventDefault(); window.currentArticlePage.setPage(${Math.max(1, this.page_ac - 1)})" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li class="page-item ${this.page_ac === 1 ? 'active' : ''}">
+                        <a class="page-link" href="#" onclick="event.preventDefault(); window.currentArticlePage.setPage(1)">1</a>
+                    </li>
+                    <li class="page-item ${this.page_ac === 2 ? 'active' : ''}">
+                        <a class="page-link" href="#" onclick="event.preventDefault(); window.currentArticlePage.setPage(2)">2</a>
+                    </li>
+                    <li class="page-item ${this.page_ac === 3 ? 'active' : ''}">
+                        <a class="page-link" href="#" onclick="event.preventDefault(); window.currentArticlePage.setPage(3)">3</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" onclick="event.preventDefault(); window.currentArticlePage.setPage(${this.page_ac + 1})" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        `; 
+    return view;
+    }
+}
+
