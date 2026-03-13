103b87d4d3489ee5eed1a53afd0a446bcdfd3a14
Gwen-Orl <gwendan.pupp-baudson@etu.univ-orleans.fr>
2026-03-13 12:22:57 +0100

    Mise en place du routeur

---
+++ SPA/js/app.js
@@ -0,0 +1,22 @@
+import Utils from "./services/Utils.js";
+import AeroportAll from "./views/pages/AeroportAll.js";
+import Home from "./views/pages/home.js";
+import Error404 from "./views/pages/Error404.js";
+
+const routes = {
+    '/' : Home,
+    '/aeroports' : AeroportAll,
+};
+
+const router = async()=>{
+    console.log("Appel router");
+    const content = null || document.querySelector('#main');
+    let request = Utils.parseRequestURL();
+    let parsedUrl = (request.resource ? '/' + request.resource : '/') + (request.id ? '/:id' : '') + (request.verb ? '/' + request.verb : '');
+    console.log(parsedUrl);
+    let page= routes[parsedUrl] ? new routes[parsedUrl] : new Error404;
+    content.innerHTML = await page.render();
+}
+
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);
