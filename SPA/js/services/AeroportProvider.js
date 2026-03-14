7928d9f5b2469d09ad2e03406ba36a1b7e2a97ae
Gwen-Orl <gwendan.pupp-baudson@etu.univ-orleans.fr>
2026-03-14 09:56:10 +0100

    Filtrage des aeroports par nom ok

--- SPA/js/services/AeroportProvider.js
+++ SPA/js/services/AeroportProvider.js
@@ -1,7 +1,7 @@
 import { AEROPORT } from "../config.js";
 
 export default class AeroportProvider{
-    static fetchAeroports = async (nb=10, page_ac=1) =>{
+    static fetchAeroports = async (nb=10, page_ac=1, nomAeroport="") =>{
         const options = {
             method : "GET",
             headers : {
@@ -10,7 +10,16 @@ export default class AeroportProvider{
         };
 
         try{
-            const response = await fetch(`${AEROPORT}?_page=${page_ac}&_per_page=${nb}`);
+            const params = new URLSearchParams({
+                _page: page_ac,
+                _per_page: nb
+            });
+
+            if (nomAeroport && nomAeroport.trim() !== "") {
+                params.set("nom_aeroport", nomAeroport.trim());
+            }
+
+            const response = await fetch(`${AEROPORT}?${params.toString()}`);
             const json = await response.json();
             return json;
         }catch(err){
