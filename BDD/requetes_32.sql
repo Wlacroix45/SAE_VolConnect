32a26517830487d5cbb9e98f62314bee25fd67d9
titouanguerrier <titouangguerrier91@gmail.com>
2026-03-16 16:53:36 +0100

    fin de la secode requete

--- BDD/requetes_32.sql
+++ BDD/requetes_32.sql
@@ -3,3 +3,12 @@ SELECT t.nom AS employe_nom,
 FROM vol v,
      TABLE(v.equipage) t
 ORDER BY t.poste;
+
+
+SELECT t.nom AS employe_nom, 
+       t.poste AS employe_poste,
+       COUNT(t.nom) AS nb_vols
+FROM vol v,
+    TABLE(v.equipage) t
+WHERE t.poste='Pilote'
+GROUP BY t.nom, t.poste;
