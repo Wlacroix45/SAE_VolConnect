c026b975135e5d384d41cce8bd4f12ad4327e3ad
Gwen-Orl <gwendan.pupp-baudson@etu.univ-orleans.fr>
2026-03-21 10:24:46 +0100

    Correction requete a) du 3.2

--- BDD/requetes_32.sql
+++ BDD/requetes_32.sql
@@ -1,8 +1,9 @@
-SELECT t.nom AS employe_nom, 
-       t.poste AS employe_poste
+SELECT v.id_vol AS vol_id,
+       t.poste AS employe_poste,
+       COUNT(*) AS nb_employes
 FROM vol v,
      TABLE(v.equipage) t
-ORDER BY t.poste;
+GROUP BY v.id_vol, t.poste;
 
 
 SELECT t.nom AS employe_nom, 
