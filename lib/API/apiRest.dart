5ccb913fb13f20dae0ce50d05c2977b051f8db08
titouanguerrier <titouangguerrier91@gmail.com>
2026-03-26 13:02:40 +0100

    ajout du reqst

--- lib/API/apiRest.dart
+++ lib/API/apiRest.dart
@@ -5,7 +5,10 @@ import 'package:flutter/physics.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/services.dart';
+import 'package:volconnect/models/aeroport.dart';
+import 'package:volconnect/models/terminal.dart';
 import '../models/compagnie.dart';
+import '../models/vol.dart';
 class MyAPIRest{
   Future<List<Compagnie>> getCompagnies() async{
     await Future.delayed(Duration(seconds: 1));
@@ -14,6 +17,36 @@ class MyAPIRest{
     final todos = <Compagnie>[];
     for(var element in json){
       todos.add(Compagnie.fromJson(element));
+    };
+    return todos;
+  }
+  Future<List<Vol>> getVols() async{
+    await Future.delayed(Duration(seconds: 1));
+    final dataString = await http.get(Uri.parse('http://127.0.0.1:5000/vols'));
+    final List<dynamic> json = jsonDecode(dataString.body);
+    final todos = <Vol>[];
+    for(var element in json){
+      todos.add(Vol.fromJson(element));
+    };
+    return todos;
+  }
+  Future<List<Terminal>> getTerminaux() async{
+    await Future.delayed(Duration(seconds: 1));
+    final dataString = await http.get(Uri.parse('http://127.0.0.1:5000/terminaux'));
+    final List<dynamic> json = jsonDecode(dataString.body);
+    final todos = <Terminal>[];
+    for(var element in json){
+      todos.add(Terminal.fromJson(element));
+    };
+    return todos;
+  }
+  Future<List<Aeroport>> getAeroports() async{
+    await Future.delayed(Duration(seconds: 1));
+    final dataString = await http.get(Uri.parse('http://127.0.0.1:5000/aeroports'));
+    final List<dynamic> json = jsonDecode(dataString.body);
+    final todos = <Aeroport>[];
+    for(var element in json){
+      todos.add(Aeroport.fromJson(element));
     };
     return todos;
   }
