1cced0bae38f3719ea8fd4bf4280965a011332b2
Gwen-Orl <gwendan.pupp-baudson@etu.univ-orleans.fr>
2026-03-29 21:10:07 +0200

    Ajout des derniers tests

--- vol/app/tests/functionnal/test_routes_localisations.py
+++ vol/app/tests/functionnal/test_routes_localisations.py
@@ -7,3 +7,8 @@ def test_post_localiser(client, aeroport_test, compagnie_test):
     """Test de la route POST /api/localisations pour créer une nouvelle localisation."""
     response = client.post(f'/api/localisations', json={"id_aeroport": aeroport_test.id_aeroport, "id_compagnie": compagnie_test.id_compagnie})
     assert response.status_code == 201
+
+def test_delete_localiser(client, localiser_test):
+    """Test de la route DELETE /api/localisations/<id_aeroport>/<id_compagnie> pour supprimer une localisation."""
+    response = client.delete(f'/api/localisations/{localiser_test.id_aeroport}/{localiser_test.id_compagnie}')
+    assert response.status_code == 204
