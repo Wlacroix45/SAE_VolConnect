2f45c13b2adaf9e267be42425f47301449460bb5
Gwen-Orl <gwendan.pupp-baudson@etu.univ-orleans.fr>
2026-03-30 17:32:09 +0200

    Fin SAE BD

--- BDD/Partie_3_2/requetes.sql
+++ BDD/Partie_3_2/requetes.sql
@@ -1,3 +1,4 @@
+-- Pour chaque vol, donner le nombre de personnes de l’équipage, par fonction
 SELECT v.id_vol AS vol_id,
        t.poste AS employe_poste,
        COUNT(*) AS nb_employes
@@ -5,7 +6,7 @@ FROM vol v,
      TABLE(v.equipage) t
 GROUP BY v.id_vol, t.poste;
 
-
+-- Pour chaque pilote, indiquer combien des vols lui sont associés
 SELECT t.nom AS employe_nom, 
        t.poste AS employe_poste,
        COUNT(t.nom) AS nb_vols
@@ -14,6 +15,7 @@ FROM vol v,
 WHERE t.poste='Pilote'
 GROUP BY t.nom, t.poste;
 
+-- Pour chaque vol, donner l’impact de chaque indice de qualité (valeur * poids)
 SELECT v.id_vol AS vol_id,
        i.nom_indice AS indice_nom,
        (i.valeur * i.poids) AS impact
@@ -21,7 +23,8 @@ FROM vol v,
      TABLE(v.indices_qualite) i;
 
 Select v.id_vol AS vol_id,
-       
+
+-- Pour chaque indice de qualité, calculer son impact moyen       
 SELECT i.nom_indice AS indice_nom,
        ROUND(AVG(i.valeur * i.poids), 3) AS impact_moyen
 FROM vol v,
